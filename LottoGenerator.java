8956441a69848a4a06ee57e8afcf5da4b398e4c4
Myung-hun Jung <mhj602@naver.com>
2016-05-14 16:56:21 +0900

    diff & bugfix

--- LottoGenerator.java
+++ LottoGenerator.java
@@ -11,8 +11,10 @@ import java.util.TreeMap;
 public class LottoGenerator{
 	public static void main(String[] args){
 		List<Integer> machine = new ArrayList<>();
-		for (int ball = 1; ball <= 45; ball++) {
+		for (int ball = 0; ball <= 45; ball++) {
+			//그냥 0부터 받는거로 바꿔봤따..
 			machine.add(ball);
+			//git diff로 확인할수있을까???? 애드전에만 확인가능 하다.
 		}
 		Collections.shuffle(machine);
 		List<Integer> balls = new ArrayList<>();
